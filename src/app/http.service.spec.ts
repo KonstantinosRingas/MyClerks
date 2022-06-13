232e0d0345fb2899130f60f211acaf0f519014df
KonstantinosRingas <ringas@thinkdesquared.com>
2022-06-13 14:17:04 +0300

    fix testing issues

--- src/app/http.service.spec.ts
+++ src/app/http.service.spec.ts
@@ -1,16 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
+// import { HttpService } from './http.service';
+
+// describe('HttpService', () => {
+//   let service: HttpService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(HttpService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
-  let service: HttpService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
-  });
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HttpService],
+    })
+  );
 
   it('should be created', () => {
+    const service: HttpService = TestBed.get(HttpService);
     expect(service).toBeTruthy();
   });
 });
